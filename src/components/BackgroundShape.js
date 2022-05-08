682f34563913f445824f199cec943eaa3be6140d
Chay Hui Xiang <75465219+chayhuixiang@users.noreply.github.com>
2022-05-08 14:23:59 +0800

    created redirects

--- src/components/BackgroundShape.js
+++ src/components/BackgroundShape.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BackgroundShape(props) {
   return (
-    <div className={`bg-black bg-opacity-50 rounded-lg w-1/2 mb-5 ${props.classProps}`}>{props.children}</div>
+    <div className={`bg-black bg-opacity-50 rounded-lg w-7/12 mb-5 ${props.classProps}`}>{props.children}</div>
   )
 }
 
